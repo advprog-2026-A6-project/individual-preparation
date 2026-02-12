6fd6f7df121e6a107fc1dddb5f720ef6ab108794
Rainmo0 <aunknown145@gmail.com>
2026-02-12 11:41:40 +0700

    renaming gradle files and adding depedencies for unit testing v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-	id 'java'
-	id 'org.springframework.boot' version '4.0.2'
-	id 'io.spring.dependency-management' version '1.1.7'
+	java
+	id ("org.springframework.boot") version "4.0.2"
+	id ("io.spring.dependency-management") version "1.1.7"
 }
 
-group = 'com.example'
-version = '0.0.1-SNAPSHOT'
-description = 'Individual preparation for the Advanced Programming course at the Faculty of Computer Science Universitas Indonesia'
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "Individual preparation for the Advanced Programming course at the Faculty of Computer Science Universitas Indonesia"
 
 java {
 	toolchain {
@@ -24,9 +24,9 @@ val webdrivermanagerVersion = "6.1.0"
 val junitJupiterVersion = "5.9.1"
 
 dependencies {
-	implementation 'org.springframework.boot:spring-boot-starter-webmvc'
-	testImplementation 'org.springframework.boot:spring-boot-starter-webmvc-test'
-	testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
 	testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
 	testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
