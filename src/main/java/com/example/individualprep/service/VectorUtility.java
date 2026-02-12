64dbe7d305e9e958a5884ab07cf660ff4188406e
Jovian12 <infernum212@gmail.com>
2026-02-12 19:27:26 +0700

    Completion method arithmetic multiply

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -24,7 +24,6 @@ public class VectorUtility {
 
     public double[] multiply(double[] v1, int x) {
         // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
     }
     
     public double dotProduct(double[] v1, double[] v2) {
